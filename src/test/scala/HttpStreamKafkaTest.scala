1530ce669f432ec00406a05be59402fee3e56386
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2017-08-28 16:52:32 +0800

    test kafka

--- src/test/scala/HttpStreamKafkaTest.scala
+++ src/test/scala/HttpStreamKafkaTest.scala
@@ -15,28 +15,38 @@ import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.Callback
 import scala.collection.mutable.ArrayBuffer
+import org.apache.kafka.clients.consumer.ConsumerRecords
 
 class HttpStreamKafkaTest {
 
 	val LINES2 = Array[String]("hello", "world", "bye", "world");
 	val ROWS2 = LINES2.map(Row(_));
 
-	@Test
-	def testKafka() {
-		//kafka conf
+	class ConsumerThread(topic: String, groupId: String, buffer: ArrayBuffer[String]) extends Thread {
+		//consumer
 		val props = new Properties();
-
-		props.put("group.id", "test");
+		props.put("group.id", groupId);
 		props.put("bootstrap.servers", "vm105:9092,vm106:9092,vm107:9092,vm181:9092,vm182:9092");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("auto.offset.reset", "latest");
+		props.put("auto.offset.reset", "earliest");
 
-		//subscribe
-		val consumer2 = new KafkaConsumer[String, String](props);
-		consumer2.subscribe(Arrays.asList("kafka-topic2"));
+		val consumer = new KafkaConsumer[String, String](props);
+		consumer.subscribe(Arrays.asList(topic));
 
-		//producer
+		override def run {
+			while (true) {
+				val records = consumer.poll(100);
+				records.foreach(record ⇒
+					println("key:" + record.key() + " value=" + record.value() + " partition:" + record.partition() + " offset=" + record.offset()));
+				buffer ++= records.map(_.value()).toSeq;
+				Thread.sleep(100);
+			}
+		}
+	}
+
+	@Test
+	def testKafka() {
 		val propsProducer = new Properties();
 
 		propsProducer.put("bootstrap.servers", "vm105:9092,vm106:9092,vm107:9092,vm181:9092,vm182:9092");
@@ -64,32 +74,21 @@ class HttpStreamKafkaTest {
 			});
 		}
 
-		Thread.sleep(5000);
-		val records = consumer2.poll(5000);
-		records.foreach(record ⇒
-			println("key:" + record.key() + " value=" + record.value() + " partition:" + record.partition() + " offset=" + record.offset()));
+		Thread.sleep(1000);
 
-		val records2 = records.map(_.value()).toSeq;
-		Assert.assertArrayEquals(LINES2.asInstanceOf[Array[Object]], records2.toArray.asInstanceOf[Array[Object]]);
-		println("~~~~~~~~~~~~~~~~~~~~~~");
+		val buffer = ArrayBuffer[String]();
+		val thread = new ConsumerThread("kafka-topic2", "g1", buffer);
+		thread.start();
+		Thread.sleep(10000);
+
+		val records = buffer.toArray;
+		thread.stop();
+		println(records.toSeq);
+		Assert.assertArrayEquals(LINES2.sorted.asInstanceOf[Array[Object]], records.sorted.asInstanceOf[Array[Object]]);
 	}
 
 	@Test
 	def testHttpStreamKafka() {
-		//kafka conf
-		val props = new Properties();
-
-		props.put("group.id", "test");
-		props.put("bootstrap.servers", "vm105:9092,vm106:9092,vm107:9092,vm181:9092,vm182:9092");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("auto.offset.reset", "latest");
-
-		val consumer2 = new KafkaConsumer[String, String](props);
-		consumer2.subscribe(Arrays.asList("kafka-topic1"));
-		val records3 = consumer2.poll(5000).map(_.value()).toSeq;
-		println(records3);
-
 		//starts a http server with a kafka receiver
 		val server = HttpStreamServer.start("/xxxx", 8080);
 
@@ -101,10 +100,15 @@ class HttpStreamKafkaTest {
 		//send ROWS2 to HttpStreamServer, and the server will forward messages to Kafka
 		client.sendRows("kafka-topic1", 1, ROWS2);
 
+		val buffer = ArrayBuffer[String]();
+		val thread = new ConsumerThread("kafka-topic1", "g2", buffer);
+		thread.start();
 		//now, fetch records from kafka
-		val records2 = consumer2.poll(5000).map(_.value()).toSeq;
+		Thread.sleep(10000);
+		val records2 = buffer.toSeq;
+		thread.stop();
 		println(records2);
-		Assert.assertArrayEquals(LINES2.asInstanceOf[Array[Object]], records2.toArray.asInstanceOf[Array[Object]]);
+		Assert.assertArrayEquals(LINES2.sorted.asInstanceOf[Array[Object]], records2.sorted.toArray.asInstanceOf[Array[Object]]);
 		server.stop();
 	}
 }
